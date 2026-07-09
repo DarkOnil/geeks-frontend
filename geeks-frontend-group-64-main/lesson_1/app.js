4fdc08c9f11adfaef9953acb464b0571a762ba4f
Azamat <162403279+DarkOnil@users.noreply.github.com>
2026-07-09 11:45:18 +0600

    Add files via upload

---
+++ geeks-frontend-group-64-main/lesson_1/app.js
@@ -0,0 +1,58 @@
+console.log("Hello World!");
+
+console.warn("Lesson 1");
+
+console.error("My first script...");
+
+// Data types
+
+// 1. String
+
+const agentName = "James";
+const agentSurname = "Bond";
+const codeName = "007";
+const organization = "MI6";
+const country = "United Kingdom";
+const newGame = "007 First Light";
+
+const fullName = agentName + " " + agentSurname;
+
+// 2. Number
+
+const firstBookYear = 1953;
+const firstMovieYear = 1962;
+const moviesCount = 25;
+const agentNumber = 7;
+const actorsCount = 6;
+const newGameReleaseYear = 2026;
+
+// 3. Boolean
+
+const isAgent = true;
+const worksForMI6 = true;
+const isVillain = false;
+const usesGadgets = true;
+const hasLicenseToKill = true;
+
+// Console
+
+console.log(fullName);
+console.log(agentName);
+console.log(agentSurname);
+console.log(codeName);
+console.log(organization);
+console.log(country);
+console.log(newGame);
+
+console.log(firstBookYear);
+console.log(firstMovieYear);
+console.log(moviesCount);
+console.log(agentNumber);
+console.log(actorsCount);
+console.log(newGameReleaseYear);
+
+console.log(isAgent);
+console.log(worksForMI6);
+console.log(isVillain);
+console.log(usesGadgets);
+console.log(hasLicenseToKill);
